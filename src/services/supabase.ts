44b12c90856f9da7f7a631187101990d82a40de9
Migueldev-gh <miguelchagasmaciel@gmail.com>
2026-05-29 19:26:15 -0300

    fix: supabase linking services

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,5 +1,16 @@
-import 'dotenv/config'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import dotenv from 'dotenv'
 import { createClient } from '@supabase/supabase-js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const cliRootEnvPath = path.resolve(__dirname, '../../.env')
+
+dotenv.config({
+  path: cliRootEnvPath
+})
 
 const supabaseUrl = process.env.SUPABASE_URL
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
